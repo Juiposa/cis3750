c081ab94dcfef778758433b90eaba1b1d7056479
Daniel <dgirotto@mail.uoguelph.ca>
2015-11-07 00:37:54 -0500

    updating files

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	var admin = true; /* admin is logged in when TRUE	 */
+	
+	document.getElementById("loginLink").onmouseover=function(){
+		document.getElementById("arrowPic").src="pics/arroworange.png";
+		document.getElementById("loginTable").style.backgroundColor = "orange";
+		document.getElementById("loginTable").style.color = "white";
+		document.getElementById("logTableCol1").style.backgroundColor = "orange";
+		document.getElementById("logTableCol2").style.backgroundColor = "orange";
+		
+		if(admin==true){
+			document.getElementById("adminMenu").style.display="block";
+		}
+		else {
+			document.getElementById("partMenu").style.display="block";
+		}
+	};
+	
+	document.getElementById("loginLink").onmouseout=function(){
+		document.getElementById("arrowPic").src="pics/arrow.png";
+		document.getElementById("loginTable").style.backgroundColor = "beige";
+		document.getElementById("loginTable").style.color = "orange";
+		document.getElementById("logTableCol1").style.backgroundColor = "beige";
+		document.getElementById("logTableCol2").style.backgroundColor = "beige";
+		
+		if(admin==true){
+			document.getElementById("adminMenu").style.display="none";
+		}
+		else{
+			document.getElementById("partMenu").style.display="none";
+		}	
+	};
+	
+
+	
+	document.getElementById("aboutLink").onclick=function(){
+		window.location.href = "about.html";
+	};
+	document.getElementById("homeLink").onclick=function(){
+		window.location.href = "index.html";
+	};
+	document.getElementById("contactLink").onclick=function(){
+		window.location.href = "contact.html";
+	};
+	document.getElementById("registerLink").onclick=function(){
+		window.location.href = "register.html";
+	};
+
+});
