e31bc537dd9e556d72858dbf09b3a94a2714b8bf
Jeffrey-David Kapp <Jeffrey-David Kapp>
2015-11-10 18:20:54 -0500

    everything is linked and working

--- wireframe_3750/script.js
+++ wireframe_3750/script.js
@@ -69,22 +69,27 @@ $(document).ready(function(){
 		window.location.href = "under_construction.html";
 	};	
 	if (document.getElementById("logOutLinkA")!=null){
-		document.getElementById("logOutLinkA").onclick=function(){
+			document.getElementById("logOutLinkA").onclick=function(){
 			localStorage.setItem("state", "none");
 			window.location.href = "index.html";
 		};
-}
-if (document.getElementById("logOutLinkP")!=null){
-		document.getElementById("logOutLinkP").onclick=function(){
-		localStorage.setItem("state", "none");
-		window.location.href = "index.html";
-	};	
-}
+	}
+	if (document.getElementById("logOutLinkP")!=null){
+			document.getElementById("logOutLinkP").onclick=function(){
+			localStorage.setItem("state", "none");
+			window.location.href = "index.html";
+		};	
+	}
 	document.getElementById("profileLink").onclick=function(){
 		window.location.href = "under_construction.html";
 	};	
 	document.getElementById("teamLink").onclick=function(){
-		window.location.href = "team_view.html";
+		var teamStatus = localStorage.getItem("teamStatus");
+		if (teamStatus == "true") {
+			window.location.href = "team_view.html";
+		} else {
+			window.location.href = "team_create.html";			
+		}
 	};	
 	document.getElementById("loginTable").onclick=function(){
 		window.location.href = "login.html";
@@ -103,6 +108,46 @@ if (document.getElementById("logOutLinkP")!=null){
 	};
 
 
+	$('#deleteBtn').on('click', function() {
+		var r = confirm("Are you sure you wish to delete your team?");
+		if( r==true ) {
+			localStorage.setItem("teamStatus", "false");
+			window.location.href = "index.html";
+		}
+	});
+	
+	$('#selectBtn').on('click', function() {
+		window.location.href = "team_view.html";
+	});
+	$('#inviterino').on('click', function() {
+		window.location.href = "invite.html";
+	});
+	$('#changeRoute').on('click', function() {
+		window.location.href = "route_select.html";
+	});
+	//invite pages
+	$('.btn-primary').on('click', function() {
+		window.location.href = "part_invite.html";
+	});
+	$('#createBtn').on('click', function () {
+		localStorage.setItem("teamStatus", "true");
+		window.location.href = "team_view.html";
+	});
+	$('#routeCreateLink').on('click', function () {
+		window.location.href = "route_create.html";
+	});
+	$('#routeDeleteLink').on('click', function () {
+		window.location.href = "route_delete.html";
+	});
+	$('#doneBtn').on('click', function() {
+		window.location.href = "route_manage.html";
+	});
+	$('.btn-warning').on('click', function() {
+		var r = confirm("Are you sure you wish to delete this route?");
+		if( r == true ) {
+			window.location.href= "route_manage.html";
+		}
+	});
 	//invite page stuff
 	$('.btn-default').on('click', function (){
 		if (invites < 4) {
@@ -119,11 +164,14 @@ if (document.getElementById("logOutLinkP")!=null){
 
 
 	function handleLogin () {
-		
+		var pass = false;
+		var role = false;
 		usernameValue= document.getElementById('username').value  ;
 		passwordValue=document.getElementById('password').value;
 		if (usernameValue.length <1 || passwordValue.length <1) {
 			alert ("Missing username or password, please re-enter");
+		} else {
+			pass = true;
 		}
 		// else if (passwordValue.length <6 && passwordValue.length >=1){
 		// 	alert ("Password is too short, please enter a password longer th");
@@ -131,17 +179,20 @@ if (document.getElementById("logOutLinkP")!=null){
 		if (usernameValue.length >1 && passwordValue.length >1 && !document.getElementById('admin').checked && !document.getElementById('part').checked ){
 			alert ("Please select a role");
 			
+		} else {
+			role = true;
 		}
-		roleValue= document.querySelector('input[name="role"]:checked').value;
-
-		if (roleValue=="administrator"){
-				localStorage.setItem("state", "admin");
-				window.location.href = "index.html";
+		if( pass == true && role == true ) {
+			roleValue= document.querySelector('input[name="role"]:checked').value;
+			if (roleValue=="administrator"){
+					localStorage.setItem("state", "admin");
+					window.location.href = "index.html";
+			}
+			else if (roleValue=="participant"){
+					localStorage.setItem("state", "part");
+					window.location.href = "index.html";
+			} 
 		}
-		else if (roleValue=="participant"){
-				localStorage.setItem("state", "part");
-				window.location.href = "index.html";
-		} 
 	}
 
 	document.getElementById("cancelButton").onclick=function(){
