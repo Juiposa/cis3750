e31789412b637196a03fa76c2c9798746c31d59c
bsaho <Grissletoe@gmail.com>
2015-11-23 14:51:41 -0500

    made sure JSON was parsing correctly, will comment better later and wait for feedback.

--- trickoreat-api/bsaho.js
+++ trickoreat-api/bsaho.js
@@ -10,12 +10,16 @@ function testSelect() {
 		url : 'trickoreat-api/select.php',
 
 		//put your query here
-		data : { query : "SELECT username,firstName,surName,email  FROM accountTable WHERE role=\"part\" AND eventName=\"trickguelph\"" },
+		data : { query : "SELECT username,firstName,surName,email,teamName FROM accountTable WHERE role=\"part\" AND eventName=\"trickguelph\"" },
 
 		datatype : 'json',
 		async : false
 	});
 
+    var  json= JSON.parse (jqXHR.responseText);
+
+    username=json[0].username;
+    console.log (username);
 
 	//return values (processed or un-processed) to caller here
 	console.log(jqXHR.responseText);
@@ -23,13 +27,29 @@ function testSelect() {
 
 function testInsert() {
 
+
+	//depends on values from the html file, using dummy values until you decide how you're going to put data in. 
+	if (eventName==undefined){
+		eventName="Super Happy Fun Time The Sequel"
+	}
+	if (location==undefined){
+		location="Guelph"
+	}
+	if (scheduledTime==undefined){
+		scheduledTime="2015-11-12"
+	}
+
+	insertString="INSERT IGNORE INTO eventTable (eventName,location,scheduledTime) VALUES" 
+	valueString= "(\"" +eventName +"\"," + "\"" +location + "\",\"" + scheduledTime + "\")"
+	queryString=insertString +valueString;
+	console.log ("Creating event string: " + queryString);
 	//POST request
 	$.ajax({
 		method : "POST",
 		url : 'trickoreat-api/insert.php',
 
 		//put your query here
-		data : { query : "INSERT INTO fuckthisshit (asshole) VALUES ('bashit')" },
+		data : { query :  queryString },
 
 		success : function() {
 			console.log("Insert success");
