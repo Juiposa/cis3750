f920d1a901d3a731c2015deeba0e7a0f74c8136d
bsaho <Grissletoe@gmail.com>
2015-11-23 12:50:45 -0500

    Added the ability to search events and return all participants involved with said event.
    Hardcoded it to only return trick or eat since that is the only event.

---
+++ trickoreat-api/bsaho.js
@@ -0,0 +1,38 @@
+
+
+function testSelect() {
+	console.log ("selecting parts");
+	//POST request
+	var jqXHR = $.ajax({
+		method : "POST",
+		url : 'trickoreat-api/select.php',
+
+		//put your query here
+		data : { query : "SELECT username,firstName,surName,email  FROM accountTable WHERE role=\"part\" AND eventName=\"trickguelph\"" },
+
+		datatype : 'json',
+		async : false
+	});
+			
+
+	//return values (processed or un-processed) to caller here
+	console.log(jqXHR.responseText);
+}
+
+function testInsert() {
+
+	//POST request
+	$.ajax({
+		method : "POST",
+		url : 'trickoreat-api/insert.php',
+
+		//put your query here
+		data : { query : "INSERT INTO fuckthisshit (asshole) VALUES ('bashit')" },
+
+		success : function() {
+			console.log("Insert success");
+		}
+	});
+}
+
+testSelect ();
