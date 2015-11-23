8c6362b6e0dc5ae6568f08b1f561ef7629e5c02d
sdeline <sdeline@mail.uoguelph.ca>
2015-11-23 18:50:22 -0500

    Added more edit functions

--- trickoreat-api/sdeline.js
+++ trickoreat-api/sdeline.js
@@ -1,36 +1,20 @@
-//FUNCTION: changeTeamName 
-//PARAMETERS:
-//teamName - A string containing current the team name
-//newTeamName - A string containing new team name
-
-function changeTeamName(teamName,newTeamName) 
-{
-
-	$.ajax({
-		method : "POST",
-		url : 'trickoreat-api/insert.php',
-		data : { query : "UPDATE teamTable SET name='newTeamName' WHERE name='teamName'" },
-
-		success : function() {
-			console.log("Update success");
-		}
-	});
-	
-}
-
-
-//FUNCTION: createTeam
-//PARAMETERS:
-//newTeamName - A string containing the team name
-//teamSize- An integer containing the # of team members
-//teamMembers - A string containing all the names of the team members delimited by commas
-//privacy - A string containing the words 'public' or 'private'
-//route - A string containing the name of the route
-//eventName - A string containg the name of the event 
+//======================================================================================================================================
+//FUNCTION: Create Team
+//======================================================================================================================================
+/*
+PARAMETERS:
+newTeamName - A string containing the team name
+teamSize- An integer containing the # of team members
+teamMembers - A string containing all the names of the team members delimited by commas
+privacy - A string containing the words 'public' or 'private'
+route - A string containing the name of the route
+eventName - A string containg the name of the event 
+*/
 
 function createTeam(newTeamName,teamSize,teamMembers,privacy,route,eventName) 
 {
-	var newQuery="INSERT INTO teamTable (teamName,teamSize,teamMembers,privacyStatus,routes,eventName) VALUES ('"+newTeamName+"',"+teamSize+",'"+teamMembers+"','"+privacy+"','"+route"','"+eventName+"');"
+	var newValues= newTeamName + "\'," + teamSize + ",\'" + teamMembers + "\',\'" + privacy + "\',\'" + route +"\',\'" + eventName;
+	var newQuery="INSERT INTO teamTable (teamName,teamSize,teamMembers,privacyStatus,routes,eventName) VALUES (\'"+newValues+"\');";
 
 		$.ajax({
 			method : "POST",
@@ -38,31 +22,165 @@ function createTeam(newTeamName,teamSize,teamMembers,privacy,route,eventName)
 			data : { query : newQuery },
 
 			success : function() {
-				console.log("Insert success");
+				console.log("Team Create success");
 			}
 		});
 }
 
 
-//FUNCTION: retriveTeamInfo
-//PARAMETERS:
-//teamName - A string containing the team name
+//======================================================================================================================================
+//FUNCTION: Edit Team Name 
+//======================================================================================================================================
+/*
+PARAMETERS:
+teamName - A string containing current the team name
+newTeamName - A string containing new team name
+*/
 
-function  retriveTeamInfo(teamName) 
+function editTeamName(teamName,newTeamName) 
 {
-
-	var jqXHR = $.ajax({
+	$.ajax({
 		method : "POST",
 		url : 'trickoreat-api/insert.php',
-		data : { query : "SELECT * FROM teamTable WHERE teamName='teamName'" },
+		data : { query : "UPDATE teamTable SET teamName=" + "'" + newTeamName + "'" + " WHERE teamName=" + "'" + teamName +"'" },
 
 		success : function() {
-			console.log("Update success");
+			console.log("Name Edit success");
+		}
+	});	
+}
+
+
+//======================================================================================================================================
+//FUNCTION: Edit Team Members
+//======================================================================================================================================
+/*PARAMETERS:
+teamName - A string containing current the team name
+newMembers- A string containing the new full list of team members dlimited by commas
+*/
+
+function editTeamMembers(teamName,newMembers) 
+{
+	$.ajax({
+		method : "POST",
+		url : 'trickoreat-api/insert.php',
+		data : { query : "UPDATE teamTable SET teamMembers=" + "'" + newMembers + "'" + " WHERE teamName=" + "'" + teamName +"'"  },
+
+		success : function() {
+			console.log("Members Edit success");
+		}
+	});	
+}
+
+
+//======================================================================================================================================
+//FUNCTION: Edit Team Size
+//======================================================================================================================================
+/*
+PARAMETERS:
+teamName - A string containing current the team name
+newSize- An integer containing the new team size
+*/
+function editTeamSize(teamName,newSize) 
+{
+	$.ajax({
+		method : "POST",
+		url : 'trickoreat-api/insert.php',
+		data : { query : "UPDATE teamTable SET teamSize=" + "'" + newSize + "'" + " WHERE teamName=" + "'" + teamName +"'"  },
+
+		success : function() {
+			console.log("Size Edit success");
+		}
+	});	
+}
+
+
+//======================================================================================================================================
+//FUNCTION: Edit Team Privacy
+//======================================================================================================================================
+/*
+PARAMETERS:
+teamName - A string containing current the team name
+newPrivacy- An string containing the either 'public' or 'private'
+*/
+function editTeamPrivacy(teamName,newPrivacy) 
+{
+	$.ajax({
+		method : "POST",
+		url : 'trickoreat-api/insert.php',
+		data : { query : "UPDATE teamTable SET privacyStatus=" + "'" + newPrivacy + "'" + " WHERE teamName=" + "'" + teamName +"'"  },
+
+		success : function() {
+			console.log("Privacy Edit success");
+		}
+	});	
+}
+
+
+//======================================================================================================================================
+//FUNCTION: Edit Team Route
+//======================================================================================================================================
+/*
+PARAMETERS:
+teamName - A string containing current the team name
+newRoute- An string containing the new route name
+*/
+function editTeamRoute(teamName,newRoute) 
+{
+	$.ajax({
+		method : "POST",
+		url : 'trickoreat-api/insert.php',
+		data : { query : "UPDATE teamTable SET routes=" + "'" + newRoute + "'" + " WHERE teamName=" + "'" + teamName +"'"  },
+
+		success : function() {
+			console.log("Route Edit success");
+		}
+	});	
+}
+
+
+//======================================================================================================================================
+//FUNCTION: Retrive Team Info
+//======================================================================================================================================
+/*
+PARAMETERS:
+teamName - A string containing the team name
+RETURNS:
+result - A json object with all columns in the team table
+*/
+function retriveTeamInfo(teamName) 
+{
+	var result = $.ajax({
+		method : "POST",
+		url : 'trickoreat-api/select.php',
+		data : { query : "SELECT * FROM teamTable WHERE teamName=" + "'" + teamName + "'" },
+
+		success : function() {
+			console.log("Retrive Team Info success");
 		}
 	});
 
-	return jqXHR;
-
+	console.log(result.teamName);
+	
+	return result;
 }
 
-createTeam("Team2",1,"Jake Blake","public","Route 222","TrickOrEatGuelph") 
+
+//======================================================================================================================================
+//FUNCTION EXAMPLES
+//======================================================================================================================================
+
+
+//createTeam("Team1",2,"Eric,Carla","public","Route 222","TrickOrEatGuelph");
+
+//editTeamName("Team1","ARR!");
+
+//editTeamMembers("Team1","James,Alex,Carl,Steve");
+
+//editTeamSize("Team1",4);
+
+//editTeamPrivacy("Team1","public");
+
+//editTeamRoute("Team1","Route 17")
+
+//retriveTeamInfo("Team1");
